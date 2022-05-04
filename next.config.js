0ac958def47aa8e061160f12399307328e6acec3
Anton <frontendevs@gmail.com>
2022-05-04 13:59:21 +0300

    first commit

---
+++ next.config.js
@@ -0,0 +1,32 @@
+// const withPWA = require('next-pwa');
+// const runtimeCaching = require('next-pwa/cache');
+
+module.exports = {
+  reactStrictMode: true,
+  i18n: {
+    locales: ['ru', 'fi', 'en'],
+    defaultLocale: 'fi',
+    localeDetection: false,
+  },
+  images: {
+    domains: ['www.datocms-assets.com'],
+  },
+};
+// module.exports = withPWA({
+//   reactStrictMode: true,
+//   pwa: {
+//     dest: 'public',
+//     register: true,
+//     skipWaiting: true,
+//     runtimeCaching,
+//     disable: process.env.NODE_ENV === 'development',
+//   },
+//   i18n: {
+//     locales: ['ru', 'fi'],
+//     defaultLocale: 'fi',
+//     localeDetection: false,
+//   },
+//   images: {
+//     domains: ['www.datocms-assets.com'],
+//   },
+// });
