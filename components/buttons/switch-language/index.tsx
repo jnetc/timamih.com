c086b4bfa34cd35525b917d756ccaf628e40725a
Anton <frontendevs@gmail.com>
2022-05-07 11:02:51 +0300

    created hook useTransitionAnimation

--- components/buttons/switch-language/index.tsx
+++ components/buttons/switch-language/index.tsx
@@ -2,6 +2,7 @@ import { useRouter } from 'next/router';
 import { useState, useRef, useEffect } from 'react';
 // Hooks
 import { useStore } from '@Hooks/useStore';
+import { useTransitionAnimation } from '@Hooks/useTransitionAnimation';
 // Component
 import { LanguageButton } from './LanguageButton';
 // Styles
@@ -13,22 +14,26 @@ export const SwitchLanguageButton = () => {
   const { asPath, locale } = useRouter();
   const { languages } = useStore();
   const ref = useRef<HTMLDivElement>(null);
-  const [showLangMenu, setShowLangMenu] = useState(false);
+  const animated = useRef<HTMLDivElement>(null);
+  const [showMenu, setShowMenu] = useState(false);
 
-  const langMenuHandler = () => setShowLangMenu(!showLangMenu);
-  const selectLanguage = () => setShowLangMenu(false);
+  const toggleLanguageMenu = () => setShowMenu(!showMenu);
+
+  useTransitionAnimation(animated, showMenu, 300);
 
   // Handle outside click
+  // Don't use hook, because need to update
+  // translation page after selected language
   useEffect(() => {
     const outsideClick = (event: Event) => {
       const element = event.target as HTMLElement;
       if (ref.current?.contains(element)) return;
-      setShowLangMenu(false);
+      setShowMenu(false);
     };
     document.addEventListener('click', outsideClick);
 
     return () => document.removeEventListener('click', outsideClick);
-  }, [showLangMenu]);
+  }, [showMenu]);
 
   const buttonsOrder = languages.map(order => {
     const reType = order as LanguagesType;
@@ -41,28 +46,20 @@ export const SwitchLanguageButton = () => {
         role="button"
         aria-label="language switcher"
         title="language switcher"
-        onClick={langMenuHandler}
+        onClick={toggleLanguageMenu}
         ref={ref}
         className={styles.module}
         id="languages"
       >
         {locale}
       </div>
-      {showLangMenu && (
-        <div className={styles.buttons} onClick={selectLanguage}>
-          {buttonsOrder}
-        </div>
-      )}
-      {/* <div
-        className={`${styles.buttons} ${
-          showLangMenu ? styles.show_languages : ''
-        }`}
-        // ref={ref}
-        onClick={selectLanguage}
+      <div
+        className={styles.buttons}
+        onClick={toggleLanguageMenu}
+        ref={animated}
       >
         {buttonsOrder}
-      </div> */}
-      {/* <div className={styles.buttons}>{buttonsOrder}</div> */}
+      </div>
     </>
   );
 };
---
+++ hooks/useTransitionAnimation.ts
@@ -0,0 +1,35 @@
+import { useEffect, RefObject } from 'react';
+export const useTransitionAnimation = (
+  ref: RefObject<HTMLElement>,
+  state: boolean,
+  duration: number
+) => {
+  useEffect(() => {
+    const el = ref.current;
+    if (!el) return;
+
+    // Entering transition
+    if (state) {
+      el.classList.add('mountOnEnter');
+
+      const entering = setTimeout(() => {
+        el.classList.add('enterActive');
+        clearTimeout(entering);
+      });
+
+      return;
+    }
+
+    // Exiting transition
+    if (!state) {
+      el.classList.add('exitActive');
+      el.classList.remove('enterActive');
+
+      const exited = setTimeout(() => {
+        el.classList.remove('exitActive');
+        el.classList.remove('mountOnEnter');
+        clearTimeout(exited);
+      }, duration);
+    }
+  }, [state, ref, duration]);
+};
