d496d3ce7aad1d172833dc39390b1718a2381418
Anton <frontendevs@gmail.com>
2022-05-23 11:21:25 +0300

    added "get in touch" illustration

--- components/get-in-touch/index.tsx
+++ components/get-in-touch/index.tsx
@@ -1,7 +1,15 @@
+import { Illustration } from './GetInTouchSVG';
 const GetInTouch = () => {
   return (
     <section id="get-in-touch" className="get-in-touch">
-      <h1 className="section-title">Get in touch we us</h1>
+      <header className="section-header">
+        <h1 className="section-title">Get in touch we us</h1>
+        <p className="description-section">
+          We recommend contacting us by phone, WhatsApp or Telegram, this will
+          significantly speed up the process!
+        </p>
+      </header>
+      <Illustration />
     </section>
   );
 };
