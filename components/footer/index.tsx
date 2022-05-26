537c1a99f35d985a3fca0e3f268bfca45fdce186
Anton <frontendevs@gmail.com>
2022-05-26 11:45:23 +0300

    refactored body paddings

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -2,9 +2,9 @@ import styles from './footer.module.css';
 
 const Footer = () => {
   return (
-    <section className={styles.module}>
+    <footer className={styles.module}>
       <p>Timamih © 2022</p>
-    </section>
+    </footer>
   );
 };
 
