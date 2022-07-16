2987182624b6e2ddb1325014eefef2f56ac506ee
Anton <frontendevs@gmail.com>
2022-07-16 16:15:56 +0300

    trying to find a strange bug in safari

--- components/what-we-offer/services/ServiceContent.tsx
+++ components/what-we-offer/services/ServiceContent.tsx
@@ -30,10 +30,10 @@ export const ServiceContent = ({ data }: { data: ServiceContentType }) => {
   return (
     <div ref={ref} className={`service-content ${showTeam ? 'show-teammates' : 'hide-teammates'}`}>
       <ServiceArticle data={article} handler={openTeammates} />
-      <span
+      {/* <span
         className="background-article"
         style={{ visibility: 'hidden', animationDuration: '0s' }}
-      />
+      /> */}
       <TeammateGroup teammates={teammates} handler={() => setShowTeam(false)} />
     </div>
   );
