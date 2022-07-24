b6b2a85c60e39e87e53ff640a66e6b934f8203b3
Anton <frontendevs@gmail.com>
2022-07-24 17:55:45 +0300

    visibility solution

--- components/what-we-offer/services/ServiceContent.tsx
+++ components/what-we-offer/services/ServiceContent.tsx
@@ -1,6 +1,7 @@
 import { useRef, useState, useEffect } from 'react';
 // Hook
 import { useOutsideClick } from '@Hooks/useOutsideClick';
+// import { useTransitionAnimation } from '@Hooks/useTransitionAnimation';
 // Component
 import { TeammateGroup } from './service-teammate/Teammate-group';
 import { ServiceArticle } from './service-article/ServiceArticle';
@@ -10,11 +11,25 @@ import { ServiceContentType } from '@Types';
 export const ServiceContent = ({ data }: { data: ServiceContentType }) => {
   const { teammates, order, ...article } = data;
   const [showTeam, setShowTeam] = useState(false);
+  const [visible, setVisible] = useState(false);
   const ref = useRef<HTMLDivElement>(null);
+  const refBG = useRef<HTMLDivElement>(null);
 
   const openTeammates = () => {
     if (!teammates || teammates.length === 0) return;
     setShowTeam(true);
+    setTimeout(() => {
+      refBG.current?.classList.remove('hidden');
+    }, 100);
+    console.log('start', visible);
+  };
+
+  const closeTeammates = () => {
+    setShowTeam(false);
+    setTimeout(() => {
+      refBG.current?.classList.remove('hidden');
+    }, 100);
+    console.log('start', visible);
   };
 
   useOutsideClick(ref, setShowTeam);
@@ -27,12 +42,27 @@ export const ServiceContent = ({ data }: { data: ServiceContentType }) => {
     }, 700);
   }, []);
 
+  useEffect(() => {
+    refBG.current;
+  }, []);
+
+  const animationEnd = () => {
+    setTimeout(() => {
+      // setVisible(false);
+      refBG.current?.classList.add('hidden');
+    }, 100);
+    console.log('end', visible);
+  };
+
   return (
     <div ref={ref} className={`service-content ${showTeam ? 'show-teammates' : 'hide-teammates'}`}>
       <ServiceArticle data={article} handler={openTeammates} />
-      <TeammateGroup teammates={teammates} handler={() => setShowTeam(false)} />
+      <TeammateGroup teammates={teammates} handler={closeTeammates} />
+
       <span
-        className="background-article"
+        ref={refBG}
+        onAnimationEnd={animationEnd}
+        className={`background-article`}
         style={{ visibility: 'hidden', animationDuration: '0s' }}
       />
     </div>
