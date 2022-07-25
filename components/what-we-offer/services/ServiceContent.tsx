045db2f9dcdbd1d19e0e71347bca640f5d2f70f5
Anton <frontendevs@gmail.com>
2022-07-25 14:03:44 +0300

    Safari glass effect with reflection: css (hack) solution

--- components/what-we-offer/services/ServiceContent.tsx
+++ components/what-we-offer/services/ServiceContent.tsx
@@ -1,7 +1,6 @@
 import { useRef, useState, useEffect } from 'react';
 // Hook
 import { useOutsideClick } from '@Hooks/useOutsideClick';
-// import { useTransitionAnimation } from '@Hooks/useTransitionAnimation';
 // Component
 import { TeammateGroup } from './service-teammate/Teammate-group';
 import { ServiceArticle } from './service-article/ServiceArticle';
@@ -11,26 +10,14 @@ import { ServiceContentType } from '@Types';
 export const ServiceContent = ({ data }: { data: ServiceContentType }) => {
   const { teammates, order, ...article } = data;
   const [showTeam, setShowTeam] = useState(false);
-  const [visible, setVisible] = useState(false);
   const ref = useRef<HTMLDivElement>(null);
-  const refBG = useRef<HTMLDivElement>(null);
 
   const openTeammates = () => {
     if (!teammates || teammates.length === 0) return;
     setShowTeam(true);
-    setTimeout(() => {
-      refBG.current?.classList.remove('hidden');
-    }, 100);
-    console.log('start', visible);
   };
 
-  const closeTeammates = () => {
-    setShowTeam(false);
-    setTimeout(() => {
-      refBG.current?.classList.remove('hidden');
-    }, 100);
-    console.log('start', visible);
-  };
+  const closeTeammates = () => setShowTeam(false);
 
   useOutsideClick(ref, setShowTeam);
 
@@ -42,26 +29,12 @@ export const ServiceContent = ({ data }: { data: ServiceContentType }) => {
     }, 700);
   }, []);
 
-  useEffect(() => {
-    refBG.current;
-  }, []);
-
-  const animationEnd = () => {
-    setTimeout(() => {
-      // setVisible(false);
-      refBG.current?.classList.add('hidden');
-    }, 100);
-    console.log('end', visible);
-  };
-
   return (
     <div ref={ref} className={`service-content ${showTeam ? 'show-teammates' : 'hide-teammates'}`}>
       <ServiceArticle data={article} handler={openTeammates} />
       <TeammateGroup teammates={teammates} handler={closeTeammates} />
 
       <span
-        ref={refBG}
-        onAnimationEnd={animationEnd}
         className={`background-article`}
         style={{ visibility: 'hidden', animationDuration: '0s' }}
       />
