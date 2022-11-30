cf717bac0a31c14af45bfc93c85938b82e225e7b
Anton <frontendevs@gmail.com>
2022-11-30 17:21:23 +0200

    created switcher urls by regular expressions

--- components/what-we-offer/services/service-teammate/Teammate.tsx
+++ components/what-we-offer/services/service-teammate/Teammate.tsx
@@ -9,19 +9,22 @@ export const ServiceTeammate = ({ data }: { data: TeammateType }) => {
   const { language } = useStore();
 
   const generateUrl = useCallback((url: string) => {
-    const urlExp = new RegExp(`w{3}[.]+[a-z]+[.]+[a-z]{2,4}`, `gm`);
+    const httpUrlExp = new RegExp(`https:\/\/+[a-z]+[.]+[a-z]{2,5}`, `gm`);
+    const wwwUrlExp = new RegExp(`^www[.]+[a-z]+[.]+[a-z]{2,5}`, `gm`);
     const telNumberExp = new RegExp(`[+]+[0-9]+`, `gm`);
     const emailExp = new RegExp(`[a-zA-Z0-9.]+@+[a-z]+[.][a-z]{2,4}`, `gm`);
 
-    switch (true) {
+    switch (url !== '') {
       case telNumberExp.exec(url) !== null:
         return `tel:${url}`;
       case emailExp.exec(url) !== null:
         return `mailto:${url}`;
-      case urlExp.exec(url) !== null:
+      case wwwUrlExp.exec(url) !== null:
         return `https://${url}`;
-      default:
+      case httpUrlExp.exec(url) !== null:
         return url;
+      default:
+        break;
     }
   }, []);
 
@@ -49,26 +52,6 @@ export const ServiceTeammate = ({ data }: { data: TeammateType }) => {
           />
         </div>
       )}
-
-      {/* <Image
-          src={data.avatar.url}
-          blurDataURL={data.avatar.blurUpThumb}
-          alt={data.title}
-          layout="fill"
-          objectFit="cover"
-        /> */}
-      {/* <Link href="#">
-          <a>
-            <Image
-              src={data.avatar.url}
-              blurDataURL={data.avatar.blurUpThumb}
-              alt={data.title}
-              layout="fill"
-              objectFit="cover"
-            />
-          </a>
-        </Link> */}
-
       <h1 className="teammate-name">{data.title}</h1>
       <h2 className="teammate-proff">{data.proff}</h2>
       <p className="teammate-desc">{data.description}</p>
