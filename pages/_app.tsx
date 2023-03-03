1300eb44d583e52e34e17e876e89cd3bca0061eb
Anton <frontendevs@gmail.com>
2023-03-03 15:01:45 +0200

    added canonical href to HEAD

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,12 +17,13 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="format-detection" content="address=no" />
         <meta name="generator" content="Next.js" />
         <meta name="google" content="notranslate" />
-        <meta name="keywords" content="..." />
+        <meta name="keywords" content="" />
         <meta name="mobile-web-app-capable" content="yes" />
 
         <meta name="theme-color" media="(prefers-color-scheme: light)" content="hsl(32, 100%, 95%)" />
         <meta name="theme-color" media="(prefers-color-scheme: dark)" content="hsl(0, 0%, 22%)" />
 
+        <link rel="canonical" href="https://timamih.com" />
         <link rel="alternate" href="https://timamih.com/ru" hrefLang="ru" />
         <link rel="alternate" href="https://timamih.com/en" hrefLang="en" />
         <link rel="icon" href="/icons/favicon.svg" type="image/svg+xml" />
