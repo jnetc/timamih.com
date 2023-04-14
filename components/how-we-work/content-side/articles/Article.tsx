d6f6221ea36e1b7d78b56ad29d22960b47129829
Anton <frontendevs@gmail.com>
2023-04-14 23:00:12 +0300

    semantic change h1 to h3

--- components/how-we-work/content-side/articles/Article.tsx
+++ components/how-we-work/content-side/articles/Article.tsx
@@ -2,7 +2,7 @@ import { ArticleType } from '@Types';
 export const Article = ({ data, active }: { data: ArticleType; active: boolean }) => {
   return (
     <article className={`hww-article ${active ? 'active' : ''}`}>
-      <h1 className="hww-article__title">{data.title}</h1>
+      <h3 className="hww-article__title">{data.title}</h3>
       <p className="description-section">{data.description}</p>
     </article>
   );
